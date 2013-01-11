3b417f8dda35085e127ecc717cfbe3f4b84fb3b1
Scot Lawrie <sblawrie@gmail.com>
2013-01-10 21:04:02 -0500

    first commit

---
+++ Competition.h
@@ -0,0 +1,13 @@
+//
+//  Competition.h
+//  PaperPlate
+//
+//  Created by Scot Lawrie on 12/2/12.
+//  Copyright (c) 2012 Scot Lawrie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Competition : NSObject
+
+@end
---
+++ Competition.m
@@ -0,0 +1,13 @@
+//
+//  Competition.m
+//  PaperPlate
+//
+//  Created by Scot Lawrie on 12/2/12.
+//  Copyright (c) 2012 Scot Lawrie. All rights reserved.
+//
+
+#import "Competition.h"
+
+@implementation Competition
+
+@end
